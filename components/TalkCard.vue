468fa1b8c56c157440bfc9a505390114ac373355
gatheluck <gatheluck+tech@gmail.com>
2020-02-20 17:04:05 +0900

    add TalkCard

---
+++ components/TalkCard.vue
@@ -0,0 +1,18 @@
+<template>
+  <b-card 
+  v-bind:title="talk.name"
+  v-bind:sub-title="talk.speaker"
+  class="mt-2">
+    <!-- urls of slide / event --> 
+    <a v-if="talk.event_url!=''" v-bind:href="talk.event_url" :class="card-link">[Event]</a>
+    <a v-if="talk.slide_url!=''" v-bind:href="talk.slide_url" :class="card-link">[Slide]</a>
+  </b-card>
+</template>
+
+<script>
+export default {
+	props:{
+		talk: Object
+  },
+};
+</script>
