6441e33c77afd5d0df0f71861f27d8dcd8bfb29c
gatheluck <gatheluck+tech@gmail.com>
2020-02-20 17:03:16 +0900

    add MemberCard

---
+++ components/MemberCard.vue
@@ -0,0 +1,33 @@
+<template>
+  <b-card class="with-shadow">
+    <b-media>
+      <b-img
+        v-if="member.image"
+        slot="aside"
+        :src="member.image"
+        width="100"
+        height="100"
+        alt="placeholder"
+      />
+      <b-img
+        v-else
+        slot="aside"
+        :src="require('~/static/image/avatar/members.jpg')"
+        width="100"
+        height="100"
+        alt="placeholder"
+      />
+      <h5 class="mt-0">{{ member.name }}</h5>
+      <p>{{ member.affiliation }}</p>
+      <a :href="member.url" target="_blank" class="btn stretched-link"></a>
+    </b-media>
+  </b-card>
+</template>
+
+<script>
+export default {
+  props: {
+    member: Object
+  }
+};
+</script>
