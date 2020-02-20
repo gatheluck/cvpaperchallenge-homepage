e1980123246f65e96a7cdaa36009825ee50676ff
gatheluck <gatheluck+tech@gmail.com>
2020-02-20 17:03:51 +0900

    add SliderCard

---
+++ components/SlideCard.vue
@@ -0,0 +1,17 @@
+<template>
+  <b-card 
+  v-bind:title="slide.title"
+  v-bind:sub-title="slide.authors"
+  class="mt-2">
+    <!-- urls of slide / event --> 
+    <a v-if="slide.url!=''" v-bind:href="slide.url" :class="card-link">[Slide]</a>
+  </b-card>
+</template>
+
+<script>
+export default {
+	props:{
+		slide: Object
+  },
+};
+</script>
