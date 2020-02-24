bc9a5811e76aafcb2ed1689cbd2688e50c7d1dee
gatheluck <gatheluck+tech@gmail.com>
2020-02-24 17:47:40 +0900

    fix bug of pagenation

--- pages/resources.vue
+++ pages/resources.vue
@@ -21,11 +21,11 @@
       <div class="mt-4">
         <h2>Slides</h2>
         <b-pagination
-        :value="1"
-        :total-rows="totalCount"
-        :per-page="5"
+		    v-model="slidesCurrentPage"
+        :total-rows="Math.ceil(slides.length/2)"
+        :per-page="slidesPerPage"
         align="center"
-        @change="handleChangePage">
+        @change="handleChangeSlidePage">
         </b-pagination>
       </div>
 
@@ -50,11 +50,11 @@
         <h2>Talks</h2>
         <!-- pagenation -->
         <b-pagination
-        :value="1"
-        :total-rows="totalCount"
-        :per-page="5"
+        v-model="talksCurrentPage"
+        :total-rows="Math.ceil(talks.length/2)"
+        :per-page="talksPerPage"
         align="center"
-        @change="handleChangePage">
+        @change="handleChangeTalksPage">
         </b-pagination>
       </div>
 
@@ -79,53 +79,74 @@ import TalkCard from "~/components/TalkCard.vue";
 import SlideCard from "~/components/SlideCard.vue";
 import axios from "axios";
 
+// used for sort by date 
+function compareByDate(a, b) { 
+  return new Date(b.date) - new Date(a.date);
+};
+
+// used for create pair
+function makePair(prev, curr) {
+  if (prev.length == 0) {
+    return [[curr]];
+  } else {
+    if (prev.slice(-1)[0].length == 1) {
+      return [
+        ...prev.slice(0, prev.length - 1),
+        [...prev.slice(-1)[0], curr]
+      ];
+    } else {
+      return [...prev, [curr]];
+    }
+  }
+} 
+
 export default {
   components: {
-    TalkCard,
     SlideCard,
+    TalkCard,
   },
   asyncData() {
-    let talks = require('~/static/data/talks.json').content;
     let slides = require('~/static/data/slides.json').content;
+    let talks = require('~/static/data/talks.json').content;
+    let slidesPerPage = 3;
+    let talksPerPage = 3;
+    let slidesNumPage = Math.ceil(Math.ceil(slides.length/2)/slidesPerPage);
+    let talksNumPage = Math.ceil(Math.ceil(talks.length/2)/talksPerPage);
     return {
       talks,
       slides,
-      isLoading: false
+      isLoading: false,
+      slidesPerPage: slidesPerPage,
+      talksPerPage: talksPerPage,
+      slidesNumPage: slidesNumPage,
+      talksNumPage: talksNumPage,
+      slidesCurrentPage: 1,
+      talksCurrentPage: 1,
     }
   },
   computed: {
-    talkPairs() {
-      return this.talks.sort((a, b) => new Date(b.date) - new Date(a.date)).reduce((prev, curr) => {
-        if (prev.length == 0) {
-          return [[curr]];
-        } else {
-          if (prev.slice(-1)[0].length == 1) {
-            return [
-              ...prev.slice(0, prev.length - 1),
-              [...prev.slice(-1)[0], curr]
-            ];
-          } else {
-            return [...prev, [curr]];
-          }
-        }
-      }, []);
-    },
     slidePairs() {
-      return this.slides.sort((a, b) => new Date(b.date) - new Date(a.date)).reduce((prev, curr) => {
-        if (prev.length == 0) {
-          return [[curr]];
-        } else {
-          if (prev.slice(-1)[0].length == 1) {
-            return [
-              ...prev.slice(0, prev.length - 1),
-              [...prev.slice(-1)[0], curr]
-            ];
-          } else {
-            return [...prev, [curr]];
-          }
-        }
-      }, []);
+      if (this.slidesCurrentPage < this.slidesNumPage){
+        return this.slides.sort(compareByDate).reduce(makePair, []).slice((this.slidesCurrentPage-1)*this.slidesPerPage, this.slidesCurrentPage*this.slidesPerPage)
+      } else {
+        return this.slides.sort(compareByDate).reduce(makePair, []).slice((this.slidesCurrentPage-1)*this.slidesPerPage)
+      }
     },
-  }
+    talkPairs() {
+      if (this.talksCurrentPage < this.talksNumPage){
+        return this.talks.sort(compareByDate).reduce(makePair, []).slice((this.talksCurrentPage-1)*this.talksPerPage, this.talksCurrentPage*this.talksPerPage)
+      } else {
+        return this.talks.sort(compareByDate).reduce(makePair, []).slice((this.talksCurrentPage-1)*this.talksPerPage)
+      }
+    },
+  },
+  methods: {
+    handleChangeSlidePage(page) {
+      this.slidesCurrentPage=page;
+    },
+    handleChangeTalkPage(page) {
+      this.talksCurrentPage=page;
+    }
+  },
 };
 </script>
