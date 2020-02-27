b362cb337c47db850e46bf22a3ca4fc315daa41a
gatheluck <gatheluck+tech@gmail.com>
2020-02-27 17:50:12 +0900

    fix typo

--- pages/index.vue
+++ pages/index.vue
@@ -1,44 +1,53 @@
 <template>
 <div>
-    <b-jumbotron header-level="4">
-      <template slot="header">
-        <b-container>
-          <div class="text-center">
-            <b-img fluid :src="require('~/static/image/logo/title.png')" width="400"/>
-          </div>
-        </b-container>
-      </template>
-
-      <template slot="lead">
-        <b-container>
-          <div class="text-center">コンピュータビジョン分野の今を映し、創り出す挑戦です。</div>
-        </b-container>
-      </template>
-
+  <b-jumbotron header-level="4">
+    <template slot="header">
       <b-container>
-        <hr class="my-4">
-
-        <div class="text-center">
-          <b-button class="main-button" to="/recruit" size="lg" variant="dark">
-            <span>メンバー募集ページ</span>
-            <font-awesome-icon icon="angle-right"/>
-          </b-button>
-        </div>
-
-		<b-row class="mt-5">
-          <b-col cols="12">
-            <news-feed :news="news" :isLoading="isLoading"/>
-          </b-col>
-        </b-row>
+      <div class="text-center">
+        <b-img fluid :src="require('~/static/image/logo/title.png')" width="400"/>
+      </div>
       </b-container>
-    </b-jumbotron>
+    </template>
 
-	<!-- contact -->
-	<!-- <b-cotainer>
+    <template slot="lead">
+      <b-container>
+        <div class="text-center">コンピュータビジョン分野の今を映し、新しいトレンドを創り出す。</div>
+      </b-container>
+    </template>
+
+    <b-container>
+      <hr class="my-4"/>
+    </b-container>
+
+    <!-- Button -->
+    <b-container>
+    <div class="text-center">
+      <b-button class="main-button" to="/recruit" size="lg" variant="dark">
+        <span>メンバー募集ページ</span>
+        <font-awesome-icon icon="angle-right"/>
+      </b-button>
+    </div>
+    </b-container>
+  </b-jumbotron>
+
+	<!-- New Feed -->
+	<b-container>
+  <div class="mt-4">
+		<b-row class="mt-5">
+      <b-col cols="12">
+        <news-feed :news="news" :isLoading="isLoading"/>
+      </b-col>
+    </b-row>
+  </div>
+	</b-container>
+
+	<!-- Contact -->
+	<b-container>
 	<div class="mt-4">
 		<h2>Contact</h2>
+    <a href="mailto:cvpaper.challenge&#64;gmail.com">cvpaper.challenge [at] gmail.com</a>
 	</div>
-	</b-cotainer> -->
+	</b-container>
 </div>
 </template>
 
