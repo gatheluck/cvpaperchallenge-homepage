593c61855d25eea75f865d06e5c1741e55ffe13e
gatheluck <gatheluck+tech@gmail.com>
2020-02-27 21:46:30 +0900

    add shadow to talk card

--- components/TalkCard.vue
+++ components/TalkCard.vue
@@ -2,7 +2,7 @@
   <b-card 
   v-bind:title="talk.name"
   v-bind:sub-title="talk.speaker"
-  class="mt-2">
+  class="mt-2 with-shadow">
     <!-- urls of slide / event --> 
     <a v-if="talk.event_url!=''" v-bind:href="talk.event_url" :class="card-link">[Event]</a>
     <a v-if="talk.slide_url!=''" v-bind:href="talk.slide_url" :class="card-link">[Slide]</a>
