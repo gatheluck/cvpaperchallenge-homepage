d8891d27d9ef297650b5e50f34df1b40249aebe5
gatheluck <gatheluck+tech@gmail.com>
2020-02-27 13:19:49 +0900

    change color

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <b-navbar toggleable="md" type="dark" variant="info">
+  <b-navbar toggleable="md" type="dark" variant="info" class="bg-dark">
     <b-container>
       <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
       <b-navbar-brand to="/">
