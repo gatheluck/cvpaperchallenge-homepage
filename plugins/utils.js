fca3323d33523291ca30f09823049501ff6dcebf
gatheluck <gatheluck+tech@gmail.com>
2020-03-05 14:24:53 +0900

    add utils.js for public utility functions

---
+++ plugins/utils.js
@@ -0,0 +1,27 @@
+
+
+function compareByDate(a, b) { 
+  return new Date(b.date) - new Date(a.date);
+};
+
+function makePair(prev, curr) {
+  if (prev.length == 0) {
+    return [[curr]];
+  } else {
+    if (prev.slice(-1)[0].length == 1) {
+      return [
+        ...prev.slice(0, prev.length - 1),
+        [...prev.slice(-1)[0], curr]
+      ];
+    } else {
+      return [...prev, [curr]];
+    }
+  }
+} 
+
+// injected functions are avaiable from all components.
+// please refere: https://qiita.com/itouuuuuuuuu/items/bcad408842c7723e7ca3 
+export default ({}, inject) => {
+  inject('compareByDate', compareByDate);
+  inject('makePair', makePair);
+};
