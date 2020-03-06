e0ec55c58880c4a982b316fff659feea96fa0622
gatheluck <gatheluck+tech@gmail.com>
2020-03-06 17:56:52 +0900

    add router info

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,18 +1,64 @@
+const pkg = require('./package')
+const axios = require('axios')
 
-export default {
+module.exports = {
   mode: 'universal',
+
   /*
   ** Headers of the page
   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'cvpaper.challenge - コンピュータビジョン分野の今を映し、新しいトレンドを創り出す',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      {
+        name: 'keywords',
+        content: 'cvpaper.challenge, cvpaper, cv, paper, challenge, computer vision, xpaper.challenge, xpaper, nlpaper.challenge, robotpaper.challenge' 
+      },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'cvpaper.challenge公式ホームページです。コンピュータビジョン分野の今を映し、新しいトレンドを創り出します。'
+      },
+      {
+        name: 'author',
+        content: 'cvpaper.challenge, Yoshihiro Fukuhara'
+      },
+      { 
+        name: 'viewport', 
+        content: 'width=device-width, initial-scale=1' 
+      },
+      {
+        property: 'og:type',
+        content: 'website'
+      },
+      {
+        property: 'og:site_name',
+        content: 'cvpaper.challenge'
+      },
+      {
+        hid: 'og:title',
+        property: 'og:title',
+        content: 'cvpaper.challenge - コンピュータビジョン分野の今を映し、新しいトレンドを創り出す'
+      },
+      {
+        hid: 'og:description',
+        property: 'og:description',
+        content: 'cvpaper.challenge公式ホームページです。コンピュータビジョン分野の今を映し、新しいトレンドを創り出します。'
+      },
+      {
+        hid: 'og:image',
+        property: 'og:image',
+        content: 'http://xpaperchallenge.org/cv/image/logo/title.png'
+      },
+      {
+        hid: 'og:url',
+        property: 'og:url',
+        content: 'http://xpaperchallenge.org/cv'
+      }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/cv/favicon.ico' }
     ]
   },
   /*
@@ -22,8 +68,7 @@ export default {
   /*
   ** Global CSS
   */
-  css: [
-  ],
+  css: [],
   /*
   ** Plugins to load before mounting the App
   */
@@ -42,16 +87,17 @@ export default {
     // Doc: https://bootstrap-vue.js.org
     'bootstrap-vue/nuxt',
     // Doc: https://axios.nuxtjs.org/usage
-	'@nuxtjs/axios',
-	// Doc: 
-	'nuxt-fontawesome',
-	'~/modules/fetchData',
+	  '@nuxtjs/axios',
+	  // Doc: 
+	  'nuxt-fontawesome',
+	  '~/modules/fetchData',
   ],
   /*
   ** Axios module configuration
   ** See https://axios.nuxtjs.org/options
   */
   axios: {
+    baseURL: process.env.API_HOST || 'http://localhost/api'
   },
   /*
   ** Build configuration
@@ -62,6 +108,10 @@ export default {
     */
     extend (config, ctx) {
     }
+  },
+
+  router: {
+    base: '/cv/'
   },
 
   fontawesome: {
