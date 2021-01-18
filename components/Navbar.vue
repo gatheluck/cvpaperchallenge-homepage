dab87ca158acf8cd1ea73d3b147466ab564bd68a
gatheluck <gatheluck+tech@gmail.com>
2021-01-18 11:39:41 +0900

    refactor: make navbar sticky to top.

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <b-navbar toggleable="md" type="dark" variant="info" class="bg-dark">
+  <b-navbar toggleable="md" type="dark" variant="info" sticky="true" class="bg-dark">
     <b-container>
       <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
       <b-navbar-brand to="/">
