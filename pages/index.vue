0cd438cb221068c1462edcf7998d18ee3b34d7b3
gatheluck <gatheluck+tech@gmail.com>
2022-08-31 22:32:22 +0900

    feat: reflect xccv to top page and Navbar.

--- pages/index.vue
+++ pages/index.vue
@@ -20,16 +20,16 @@
     </b-container>
 
     <!-- Button -->
-    <!-- 
+    
 	<b-container>
     <div class="text-center mb-3">
       <b-button class="main-button" to="/xccv" size="lg" variant="primary">
-        <span>PRMU/CVIM RMP x cvpaper.challenge<br>研究メンバー募集ページ</span>
+        <span>MIRU MP x cvpaper.challenge<br>研究メンバー募集ページ</span>
         <font-awesome-icon icon="angle-right"/>
       </b-button>
     </div>
     </b-container>
-	-->
+	
 
     <b-container>
     <div class="text-center">
